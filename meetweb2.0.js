f3d7830ee0c78537aed1c9242493743e3b89ded7
osamah21-meet <57598420+osamah21-meet@users.noreply.github.com>
2020-02-23 11:09:52 -0500

    Add files via upload

---
+++ meetweb2.0.js
@@ -0,0 +1,96 @@
+var all=[];
+var final=[];
+
+
+
+function asian(){
+	for (const elem of all){
+		if (origin=="asian"){
+			final.push(elem);
+		}
+	}
+}
+
+function europian(){
+	for (const elem of all){
+		if (origin=="europian"){
+			final.push(elem);
+		}
+	}
+}
+
+function mideastern(){
+	for (const elem of all){
+		if (origin=="mideastern"){
+			final.push(elem);
+		}
+	}
+}
+
+function veg(){
+	for (const elem of all){
+		if (type=="veg"){
+			final.push(elem);
+		}
+	}
+}
+
+function nonveg(){
+	for (const elem of all){
+		if (type=="nonveg"){
+			final.push(elem);
+		}
+	}
+}
+
+function time10(){
+	for (const elem of all){
+		if (time==10){
+			final.push(elem);
+		}
+	}
+}
+
+function time30(){
+	for (const elem of all){
+		if (time==30){
+			final.push(elem);
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+var Tofu_VeggieStirFry=                   {origin:"asian", type:"veg",time:10,link:"https://www.youtube.com/watch?v=xye54WGqcBM"};
+var Noodles=                              {origin:"asian", type:"veg",time:10,link:"https://www.youtube.com/watch?v=dP7AYsqcT1k"};
+var Egg_rolls=                            {origin:"asian", type:"veg",time:30,link:"https://www.youtube.com/watch?v=fEF0HaBP4as"};
+var Miso_soup=                            {origin:"asian", type:"veg",time:30,link:"https://www.youtube.com/watch?v=lYDRX83-IRo"};
+var Sesame_chicken=                       {origin:"asian", type:"nonveg",time:10,link:"https://www.youtube.com/watch?v=C2CVlvSSFO4"};
+var Chinese_dumplings=                    {origin:"asian", type:"nonveg",time:10,link:"https://www.youtube.com/watch?v=J6xFRlG5Qxg"};
+var Asian_chicken_wings=                  {origin:"asian", type:"nonveg",time:30,link:"https://www.youtube.com/watch?v=17F3zVsSuyU"};
+var Chicken_manchurian=                   {origin:"asian", type:"nonveg",time:30,link:"https://www.youtube.com/watch?v=I_6aMZwf9CU"};
+var Maqloubeh=                            {origin:"mideastern", type:"veg",time:10,link:"https://www.youtube.com/watch?v=lkzR-zcWV0Q"};
+var Mujadara=                             {origin:"mideastern", type:"veg",time:10,link:"https://www.youtube.com/watch?v=I89iliR0RhU"};
+var RedLentilKebab=                       {origin:"mideastern", type:"veg",time:30,link:"https://www.youtube.com/watch?v=94VdtP5OlZI"};
+var mousakka=                             {origin:"mideastern", type:"veg",time:30,link:"https://www.youtube.com/watch?v=ozBU0lT9DSM"};
+var Musakhan=                             {origin:"mideastern", type:"nonveg",time:10,link:"https://www.youtube.com/watch?v=WMEjf74CF7E"};
+var kosa=                                 {origin:"mideastern", type:"nonveg",time:10,link:"https://www.youtube.com/watch?v=M0uWVPh8CDs"};
+var Falafel=                              {origin:"mideastern", type:"nonveg",time:30,link:"https://www.youtube.com/watch?v=foB6bxhZYF0"};
+var Hummus=                               {origin:"mideastern", type:"nonveg",time:30,link:"https://www.youtube.com/watch?v=IRECUDMZteA"};
+var Lasagna=                              {origin:"european", type:"veg",time:10,link:"https://www.youtube.com/watch?v=wkbYh1z82b4"};
+var Ratatouille=                          {origin:"european", type:"veg",time:10,link:"https://www.youtube.com/watch?v=BFdQUgAFtGU&list=PLPXxNwplOM5ld-L0nHZrCuWKk6tthdI2g"};
+var pizza=                                {origin:"european", type:"veg",time:30,link:"https://www.youtube.com/watch?v=nXO2T9rXGEI"};
+var Potato_Dumplings=                     {origin:"european", type:"veg",time:30,link:"https://www.youtube.com/watch?v=3IZFd0l4vQM"};
+var Steak Tartare=          			  {origin:"european", type:"nonveg",time:10,link:"https://www.youtube.com/watch?v=aPde98NbJ0s&list=PLPXxNwplOM5ld-L0nHZrCuWKk6tthdI2g&index=38"};
+var chicken_chasseur =                    {origin:"european", type:"nonveg",time:10,link:"https://www.youtube.com/watch?v=AES4IJ3fA-I&list=PLPXxNwplOM5ld-L0nHZrCuWKk6tthdI2g&index=57"};
+var Orange Duck Recipe=                   {origin:"european", type:"nonveg",time:30,link:"https://www.youtube.com/watch?v=15ua-qSqHMg&list=PLPXxNwplOM5ld-L0nHZrCuWKk6tthdI2g&index=20"};
+var Endive_mash_and_Dutch_meatballs=      {origin:"european", type:"nonveg",time:30,link:"https://www.youtube.com/watch?v=95hF0Qrjk58&list=PLPXxNwplOM5ld-L0nHZrCuWKk6tthdI2g&index=36"};
+
+
+
